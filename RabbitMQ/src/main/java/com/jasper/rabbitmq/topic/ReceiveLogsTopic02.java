618cf283e00b30bfe5f12ff4a45eb177efc2527f
Jasper <jasper.ckj@gmail.com>
2021-09-23 16:29:01 +0800

    [new] topic queue

---
+++ RabbitMQ/src/main/java/com/jasper/rabbitmq/topic/ReceiveLogsTopic02.java
@@ -0,0 +1,37 @@
+package com.jasper.rabbitmq.topic;
+
+import com.jasper.rabbitmq.utils.RabbitMqUtils;
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.DeliverCallback;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @author jasper
+ * @create 2021-09-23 16:18
+ */
+public class ReceiveLogsTopic02 {
+    private static final String EXCHANGE_NAME = "topic_logs";
+
+    public static void main(String[] args) throws Exception {
+        Channel channel = RabbitMqUtils.getChannel();
+        channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.TOPIC);
+        //声明 Q2 队列与绑定关系
+        String queueName = "Q2";
+        //声明
+        channel.queueDeclare(queueName, false, false, false, null);
+        //绑定
+        channel.queueBind(queueName, EXCHANGE_NAME, "*.*.rabbit");
+        channel.queueBind(queueName, EXCHANGE_NAME, "lazy.#");
+
+        System.out.println("等待接收消息........... ");
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
+            System.out.println(" 接收队列:" + queueName + " 绑定键:" + delivery.getEnvelope().getRoutingKey() + ",消息:" + message);
+        };
+        channel.basicConsume(queueName, true, deliverCallback, consumerTag -> {
+        });
+    }
+}
