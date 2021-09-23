8ac43a90f9520bf02303cab53d4ab93b9496528f
Jasper <jasper.ckj@gmail.com>
2021-09-23 16:29:26 +0800

    [new] dead queue

---
+++ RabbitMQ/src/main/java/com/jasper/rabbitmq/dead/Consumer02.java
@@ -0,0 +1,35 @@
+package com.jasper.rabbitmq.dead;
+
+import com.jasper.rabbitmq.utils.RabbitMqUtils;
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.DeliverCallback;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @author jasper
+ * @create 2021-09-23 16:23
+ */
+public class Consumer02 {
+    private static final String DEAD_EXCHANGE = "dead_exchange";
+
+    public static void main(String[] args) throws Exception {
+        Channel channel = RabbitMqUtils.getChannel();
+
+        //声明交换机
+        channel.exchangeDeclare(DEAD_EXCHANGE, BuiltinExchangeType.DIRECT);
+        //声明队列
+        String deadQueue = "dead-queue";
+        channel.queueDeclare(deadQueue, false, false, false, null);
+        channel.queueBind(deadQueue, DEAD_EXCHANGE, "lisi");
+
+        System.out.println("等待接收死信消息........... ");
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
+            System.out.println("Consumer02 接收到消息" + message);
+        };
+        channel.basicConsume(deadQueue, true, deliverCallback, consumerTag -> {
+        });
+    }
+}
